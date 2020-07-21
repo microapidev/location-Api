ed52099b04f5372f2fbd4641aefc2b9d03aea894
Oluwadamilola Sonaike <56271955+damiso15@users.noreply.github.com>
2020-07-21 12:14:33 +0100

    codebase

--- core/wsgi.py
+++ core/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for location_microapi project.
+WSGI config for core project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+from dotenv import load_dotenv
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'location_microapi.settings')
-
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 application = get_wsgi_application()
+
+project_folder = os.path.expanduser('~/core')
+load_dotenv(os.path.join(project_folder, '.env'))
