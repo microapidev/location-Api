693261c6eb16795b7608dab68e10c7199cfd4b85
damiso15 <damiso15@yahoo.com>
2020-07-25 17:08:21 +0100

    6 created custom end points

--- api/urls.py
+++ api/urls.py
@@ -1,13 +1,19 @@
 from django.urls import path
-from .views import CountryView, RegionView, ContinentView, SubRegionView, CityView, DistrictView
+from .views import CountryView, RegionView, ContinentView, SubRegionView, CityView, DistrictView, ContinentDetailView, \
+    CountryDetailView, RegionDetailView, SubRegionDetailView, CityDetailView, DistrictDetailView
 
 
 urlpatterns = [
     path('continent/', ContinentView.as_view()),
+    path('continent/<slug:slug>/', ContinentDetailView.as_view()),
     path('country/', CountryView.as_view()),
+    path('country/<slug:slug>/', CountryDetailView.as_view()),
     path('region/', RegionView.as_view()),
+    path('region/<int:id>/', RegionDetailView.as_view()),
     path('subregion/', SubRegionView.as_view()),
+    path('subregion/<int:id>/', SubRegionDetailView.as_view()),
     path('city/', CityView.as_view()),
+    path('city/<int:id>/', CityDetailView.as_view()),
     path('district/', DistrictView.as_view()),
+    path('district/<int:id>/', DistrictDetailView.as_view()),
 ]
-
--- api/views.py
+++ api/views.py
@@ -1,4 +1,5 @@
 import os
+from django.http import HttpResponse
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from dotenv import load_dotenv
@@ -34,6 +35,21 @@ class ContinentView(APIView):
                             status=status.HTTP_401_UNAUTHORIZED)
 
 
+class ContinentDetailView(APIView):
+
+    def get_object(self, slug):
+        try:
+            return Continent.objects.get(slug=slug)
+
+        except Continent.DoesNotExist:
+            return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+
+    def get(self, request, slug):
+        continent = self.get_object(slug)
+        serializer = ContinentSerializer(continent)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
 class CountryView(APIView):
     permission_classes = [IsOwnerOrReadOnly]
 
@@ -49,6 +65,21 @@ class CountryView(APIView):
         else:
             return Response({'error': 'you are not authorized to perform this action.'},
                             status=status.HTTP_401_UNAUTHORIZED)
+
+
+class CountryDetailView(APIView):
+
+    def get_object(self, slug):
+        try:
+            return Country.objects.get(slug=slug)
+
+        except Country.DoesNotExist:
+            return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+
+    def get(self, request, slug):
+        country = self.get_object(slug)
+        serializer = CountrySerializer(country)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 class RegionView(APIView):
@@ -68,6 +99,21 @@ class RegionView(APIView):
                             status=status.HTTP_401_UNAUTHORIZED)
 
 
+class RegionDetailView(APIView):
+
+    def get_object(self, id):
+        try:
+            return Region.objects.get(id=id)
+
+        except Region.DoesNotExist:
+            return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+
+    def get(self, request, id):
+        region = self.get_object(id)
+        serializer = RegionSerializer(region)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
 class SubRegionView(APIView):
     permission_classes = [IsOwnerOrReadOnly]
 
@@ -83,6 +129,21 @@ class SubRegionView(APIView):
         else:
             return Response({'error': 'you are not authorized to perform this action.'},
                             status=status.HTTP_401_UNAUTHORIZED)
+
+
+class SubRegionDetailView(APIView):
+
+    def get_object(self, id):
+        try:
+            return Subregion.objects.get(id=id)
+
+        except Subregion.DoesNotExist:
+            return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+
+    def get(self, request, id):
+        sub_region = self.get_object(id)
+        serializer = SubRegionSerializer(sub_region)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 class CityView(APIView):
@@ -102,6 +163,21 @@ class CityView(APIView):
                             status=status.HTTP_401_UNAUTHORIZED)
 
 
+class CityDetailView(APIView):
+
+    def get_object(self, id):
+        try:
+            return City.objects.get(id=id)
+
+        except City.DoesNotExist:
+            return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+
+    def get(self, request, id):
+        city = self.get_object(id)
+        serializer = CitySerializer(city)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
 class DistrictView(APIView):
     permission_classes = [IsOwnerOrReadOnly]
 
@@ -117,3 +193,18 @@ class DistrictView(APIView):
         else:
             return Response({'error': 'you are not authorized to perform this action.'},
                             status=status.HTTP_401_UNAUTHORIZED)
+
+
+class DistrictDetailView(APIView):
+
+    def get_object(self, id):
+        try:
+            return District.objects.get(id=id)
+
+        except District.DoesNotExist:
+            return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+
+    def get(self, request, id):
+        district = self.get_object(id)
+        serializer = DistrictSerializer(district)
+        return Response(serializer.data, status=status.HTTP_200_OK)
